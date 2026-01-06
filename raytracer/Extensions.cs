9c5c310da0a1c33b9aed8125a6a81265541c94bf
Kezak1 <kenguyen701@gmail.com>
2026-01-06 17:53:56 +0100

    deleted unessesary usings

--- raytracer/Extensions.cs
+++ raytracer/Extensions.cs
@@ -1,5 +1,3 @@
-using System.Runtime.InteropServices;
-
 namespace raytracer;
 
 public static class Extensions {
--- raytracer/Material.cs
+++ raytracer/Material.cs
@@ -1,5 +1,3 @@
-using System.Reflection.Emit;
-
 namespace raytracer;
 
 public abstract class Material
